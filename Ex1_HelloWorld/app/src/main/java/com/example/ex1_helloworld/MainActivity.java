92dca532a45feffc33d71fb66cb0a0c9e9801b04
pg2311 <pro.gim.2311@gmail.com>
2025-12-01 17:31:04 +0900

    init

---
+++ Ex1_HelloWorld/app/src/main/java/com/example/ex1_helloworld/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.ex1_helloworld;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.textView);
+        Button bt = findViewById(R.id.button2);
+
+        tv.setText("첫번째 TextView");
+        bt.setText("첫번째 Button");
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
